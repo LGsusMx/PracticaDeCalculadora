b6c4f584aebcdc69bd38cd816990b1af1c6a3c88
LGsusMx <caednicolas2@gmail.com>
2020-11-26 10:27:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calculatorpractice
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/calculatorpractice)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CalculatorComponent } from "../calculator/calculator.component";
+import { NgxsModule } from "@ngxs/store";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgxsModule.forRoot([], { developmentMode: true })
+  ],
+  declarations: [AppComponent, HelloComponent, CalculatorComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
